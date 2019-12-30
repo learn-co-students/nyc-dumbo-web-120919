c18a058e494ed923303d161e2a7f8b7940edf6b6
Ian Hollander <ianholla@gmail.com>
2019-12-30 15:12:17 -0500

    aftter hashes and the internet

--- 12-hashes-and-the-internet/README.md
+++ 12-hashes-and-the-internet/README.md
@@ -7,11 +7,26 @@
 * Server responses
 * Developer console
 * APIs
+- Application Programming Interface
+
+class Book
+  def title
+    @title
+  end
+
+  private
+  def something
+    "something else"
+  end
+
+end
+
 * JSON
 
 ### Deliverables
 
 * Write an application that takes a search term from a user
 * Make a Request to the GoogleBooks API and get back some results
+  * Docs: https://developers.google.com/books/docs/v1/using#PerformingSearch
   * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
-* Display the titles, author names, and description for each book
+* Display the titles, author names joined by an &, and first 100 characters of the description for each book
--- 12-hashes-and-the-internet/app.rb
+++ 12-hashes-and-the-internet/app.rb
@@ -1,5 +1,46 @@
 # 'https://www.googleapis.com/books/v1/volumes?q=ruby+programming'
 
 require 'rest-client'
-require 'json'
+# faraday, httparty
+# require 'json'
 require 'pry'
+
+# * Write an application that takes a search term from a user
+puts "Welcome to our Book Searcher!"
+puts "Please enter a search term:"
+search_term = gets.chomp
+
+# * Make a Request to the GoogleBooks API and get back some results
+url = "https://www.googleapis.com/books/v1/volumes?q=#{search_term}"
+
+response = RestClient.get url
+body = response.body
+parsed_body = JSON.parse(body)
+
+# * Display the titles, author names joined by an &, and first 100 characters of the description for each book
+books = parsed_body["items"]
+books.each do |book_hash|
+  title = book_hash["volumeInfo"]["title"]
+  authors = book_hash["volumeInfo"]["authors"]
+  description = book_hash["volumeInfo"]["description"]
+
+  if authors.nil?
+    author_text = "No Authors"
+  else
+    author_text = authors.join(" & ")
+  end
+
+  if description.nil?
+    description_text = "No description"
+  else
+    description_text = description[0...100]
+  end
+
+  puts "Title: #{title}"
+  puts "Author: #{author_text}"
+  puts "Description: #{description_text}..."
+  puts "*" * 15
+end
+
+#   * Docs: https://developers.google.com/books/docs/v1/using#PerformingSearch
+#   * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
