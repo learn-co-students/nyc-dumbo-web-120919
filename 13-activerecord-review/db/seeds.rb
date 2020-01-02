6403bba6bcd3be2ccf021d47396bde97460e0ec9
ihollander <ianholla@gmail.com>
2020-01-02 07:11:13 -0500

    before ar review

---
+++ 13-activerecord-review/db/seeds.rb
@@ -0,0 +1,36 @@
+puts "Clearing old data..."
+Magazine.destroy_all
+Reader.destroy_all
+
+puts "Seeding magazines..."
+magazines = ["The New Yorker", "National Geographic", "Wired", "People", "Cosmopolitan", "Time", "US Weekly", "New York Magazine"]
+# Create sample magazines
+magazines.each do |magazine|
+  Magazine.create(title: magazine)
+end
+
+puts "Seeding readers..."
+# Create 10 random Readers
+10.times do
+  Reader.create(name: Faker::Name.name, email: Faker::Internet.email)
+end
+
+puts "Seeding subscriptions..."
+# Create 20 random subscriptions
+20.times do
+  # Get a reader instance:
+  reader = Reader.all.sample
+  # Get a magazine instance:
+  magazine = Magazine.all.sample
+  # Get a random price between 10 and 100:
+  price = rand(10..100)
+
+  # ****************************************************************************
+  # * TODO: create subscriptions! Remember, a subscription belongs to a reader *
+  # * and a subscription belongs to a magazine.                                *
+  # ****************************************************************************
+  # Create Subscriptions Here
+  
+end
+
+puts "Done!"
