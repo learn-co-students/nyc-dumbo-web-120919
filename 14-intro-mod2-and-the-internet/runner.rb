c3c21afc9037f074c8d758a52b2f145e1a6389a3
Dwire <gjdwyer8@gmail.com>
2020-01-06 12:55:00 -0500

    donzo

---
+++ 14-intro-mod2-and-the-internet/runner.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+get '/' do 
+  'hi'  
+end
+
+get '/profile' do 
+  # Do some logic 
+
+  'IT IS ALL A ABOUT YOU'  
+end
+
+# post "/users" do 
+#  user = User.create(name: 'Greg')
+
+#   "Hey #{user.name}, how are you"
+# end
+
+
+
